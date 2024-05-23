32aba52175cf4948e18d59e58a95e2b30aa41844
Brian <galyenb@vcu.edu>
2024-05-23 16:02:30 -0400

    removed className as prop for LogoIcon

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -12,12 +12,7 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
         }
       )}
     >
-      <LogoIcon
-        className={clsx({
-          'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
-        })}
-      />
+      <LogoIcon />
     </div>
   );
 }
