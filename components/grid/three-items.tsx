514bd2175154c563c1364b6675d5cb1c6253a9a7
Brian <galyenb@vcu.edu>
2024-05-23 13:57:10 -0400

    fixed frontpage handle on collection

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -2,6 +2,7 @@ import { GridTileImage } from 'components/grid/tile';
 import { getCollectionProducts } from 'lib/shopify';
 import type { Product } from 'lib/shopify/types';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 function ThreeItemGridItem({
   item,
@@ -40,7 +41,7 @@ function ThreeItemGridItem({
 export async function ThreeItemGrid() {
   // Collections that start with `hidden-*` are hidden from the search page.
   const homepageItems = await getCollectionProducts({
-    collection: 'hidden-homepage-featured-items'
+    collection: 'frontpage'
   });
 
   if (!homepageItems[0] || !homepageItems[1] || !homepageItems[2]) return null;
@@ -49,7 +50,9 @@ export async function ThreeItemGrid() {
 
   return (
     <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
-      <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
+      <Suspense>
+        <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
+      </Suspense>
       <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
       <ThreeItemGridItem size="half" item={thirdProduct} />
     </section>
