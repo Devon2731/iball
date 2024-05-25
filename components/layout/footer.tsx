89f2df5091e8ac93ccb3938459c50cfd81ac3775
Devon2731 <devon.whitaker27@gmail.com>
2024-05-24 20:28:05 -0400

    modified footer from  Califorina to Virginia

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -45,7 +45,7 @@ export default async function Footer() {
             {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
-          <p>Designed in California</p>
+          <p>Designed in Virginia</p>
           <p className="md:ml-auto">
             <a href="https://vercel.com" className="text-black dark:text-white">
               Crafted by SeeYouThursday 😎
