722440c05883813aca89daf23f02688a93236ddb
Brian <galyenb@vcu.edu>
2024-05-31 10:51:10 -0400

    fixed pricing with Hoodies using conditional in PriceFilter Component

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,5 +1,6 @@
 import { AddToCart } from 'components/cart/add-to-cart';
-import Price from 'components/price';
+// import Price from 'components/price';
+import { PriceFilter } from 'components/product/price-filter';
 import Prose from 'components/prose';
 import { Product } from 'lib/shopify/types';
 import { Suspense } from 'react';
@@ -11,10 +12,7 @@ export function ProductDescription({ product }: { product: Product }) {
       <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
         <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
         <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
-          <Price
-            amount={product.priceRange.maxVariantPrice.amount}
-            currencyCode={product.priceRange.maxVariantPrice.currencyCode}
-          />
+          <PriceFilter product={product} />
         </div>
       </div>
       <Suspense fallback={null}>
