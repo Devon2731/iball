471b876b2b122305c3ee605fb99146ac7efaccb2
Devon2731 <devon.whitaker27@gmail.com>
2024-05-31 14:16:47 -0400

    fixed the sitution by going into the hero folder where the index.js was and added overflow-x-hidden in the div class to get rid of the horizontial scroll

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,25 +1,12 @@
 'use client';
 import { ParallaxBanner } from 'react-scroll-parallax';
 import { HeroWords } from './hero-text/index';
+
 const Hero = () => {
   return (
-    <div
-      className="w-screen"
-      // style={{
-      //   backgroundImage: `url('/iball-hero.svg')`,
-      //   backgroundRepeat: 'no-repeat',
-      //   backgroundPosition: 'center'
-      // }}
-    >
+    <div className="w-screen overflow-x-hidden">
       <HeroWords />
-      {/* <Image
-        src="/no-lines-hero.png"
-        height={400}
-        width={400}
-        quality={100}
-        alt="basketball hero image"
-        className="lg:h-88 relative mb-24 mt-1 w-screen"
-      /> */}
+
       <ParallaxBanner
         layers={[{ image: '/outside-hoop.png', speed: -15 }]}
         className="mb-10 mt-1 aspect-[2/1] sm:mb-12 sm:aspect-[3/1]"
