b0dcb023de5bb6c87c35005bb2bb457b0a025cd0
Devon2731 <devon.whitaker27@gmail.com>
2024-06-12 16:39:15 -0400

    fixed padding intitle and also linked the training session to the homepage button also added padding to quotes in the homepage

--- components/layout/hero/hero-text/index.tsx
+++ components/layout/hero/hero-text/index.tsx
@@ -1,17 +1,22 @@
 'use client';
 import { TextGenerateEffect } from 'components/ui/text-generate-effect';
 
-const words = ` THE LAB`;
+const The = `THE`;
+const Lab = `LAB`;
 
 export function HeroWords() {
   return (
     <div className="mb-3 ms-4 flex items-center justify-center lg:left-1/3 lg:top-1/2">
       {/* <TextGenerateEffect words={words} className="" />
       <TextGenerateEffect words={thrLine} className="" /> */}
-      <p className="kode-mono-lab mt-2 pe-3 text-3xl text-black md:text-5xl xl:pe-9 xl:text-7xl dark:text-white">
-        WELCOME TO<br></br>
+      <p className="kode-mono-lab mt-2 text-balance pe-2 ps-2 text-3xl text-black md:text-5xl xl:pe-4 xl:text-7xl dark:text-white">
+        WELCOME
       </p>
-      <TextGenerateEffect words={words} className="" />
+      <p className="kode-mono-lab mt-2 text-balance pe-1 ps-1 text-3xl text-black md:text-5xl xl:pe-4 xl:text-7xl dark:text-white">
+        TO
+      </p>
+      <TextGenerateEffect words={The} className="pe-2 ps-2" />
+      <TextGenerateEffect words={Lab} className="ps-2" />
     </div>
   );
 }
