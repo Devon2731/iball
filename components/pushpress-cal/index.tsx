60e36182f58a4198dff4237b7a3c4738418415b4
Brian <galyenb@vcu.edu>
2024-06-24 15:33:38 -0400

    cleaned up commented code in pushpress components

--- components/pushpress-cal/index.tsx
+++ components/pushpress-cal/index.tsx
@@ -5,12 +5,8 @@ export const PushPressCal = () => {
         <iframe
           title="iBall247 Training Calendar"
           src="https://iball247.pushpress.com/open/calendar?framed=0"
-          // style={{ width: '500px', height: '400px' }}
           className="h-[600px] w-[370px] max-w-[560px] md:w-[560px]"
-          // height={500}
-          // width={560}
         >
-          {/* Removed the <noframes> tag */}
           {/* Consider providing alternative content here if necessary */}
         </iframe>
       </div>
